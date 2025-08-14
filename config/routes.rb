5c3ce1f16c121b4f4b5d71985adb27fec99e547f
charles frechet <charles.frechet@gmail.com>
2025-08-14 17:02:15 +0200

    rails watch list 1

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+#          Prefix Verb   URI Pattern                            Controller#Action
+#     restaurants GET    /restaurants(.:format)                 restaurants#index
+#  new_restaurant GET    /restaurants/new(.:format)             restaurants#new
+#                 POST   /restaurants(.:format)                 restaurants#create
+#      restaurant GET    /restaurants/:id(.:format)             restaurants#show
+# edit_restaurant GET    /restaurants/:id/edit(.:format)        restaurants#edit
+#                 PATCH  /restaurants/:id(.:format)             restaurants#update
+#                 DELETE /restaurants/:id(.:format)             restaurants#destroy
